64affa26d4e5ff9b9cfb2fdfc5767b9e65ae70cc
Jani Reinikainen <reinikai@users.noreply.github.com>
2017-05-09 15:41:38 +0300

    Implement function for setting dragon stats

--- mugloar/dragon.py
+++ mugloar/dragon.py
@@ -1,29 +1,26 @@
-
 class Dragon:
 
-    stats_map = {'attack': 'scaleThickness',
+    # Match knight's stats with dragon's stats.
+    STATS_MAP = {'attack': 'scaleThickness',
                  'armor': 'clawSharpness',
                  'agility': 'wingStrength',
                  'endurance': 'fireBreath'}
 
     def __init__(self, weather_code, knight):
+        self.dragon_stats = {}
+
         if weather_code == 'T E':
             # Draught requires a 'balanced' dragon.
-            self.scaleThickness = 5
-            self.clawSharpness = 5
-            self.wingStrength = 5
-            self.fireBreath = 5
+            self.set_dragon_stats(5, 5, 5, 5)
         elif weather_code == 'FUNDEFINEDG':
             # Fog means we're unseen, no need to fly.
-            self.scaleThickness = 8
-            self.clawSharpness = 8
-            self.wingStrength = 0
-            self.fireBreath = 4
+            self.set_dragon_stats(8, 8, 0, 4)
         elif weather_code == 'NMR':
             # Remove name (string) so that sorting of integers works as expected.
             del knight['name']
-            knight = sorted(knight.items(), key=lambda x: x[1],
-                            reverse=True)
+
+            # Sort opponent's stats from high to low so that we can match points.
+            knight = sorted(knight.items(), key=lambda x: x[1], reverse=True)
 
             # This algorithm typically delivers a success ratio of over 80%.
             points_left = 20
@@ -32,41 +29,37 @@ class Dragon:
 
                 if i == 0:
                     points = value + 2 if points_left >= value + 2 else points_left
-                    setattr(self, self.stats_map[stat], points)
+                    self.dragon_stats[self.STATS_MAP[stat]] = points
                 elif i == 1:
                     if value < 3:
-                        setattr(self, self.stats_map[stat], 0)
+                        self.dragon_stats[self.STATS_MAP[stat]] = 0
                     else:
                         points = value - 2 if points_left >= value - 2 else points_left
-                        setattr(self, self.stats_map[stat], points)
+                        self.dragon_stats[self.STATS_MAP[stat]] = points
                 elif i == 2:
                     points = value + 1 if points_left >= value + 1 else points_left
-                    setattr(self, self.stats_map[stat], points)
+                    self.dragon_stats[self.STATS_MAP[stat]] = points
                 else:
                     if value < 2:
-                        setattr(self, self.stats_map[stat], 0)
+                        self.dragon_stats[self.STATS_MAP[stat]] = 0
                     else:
                         points = value - 1 if points_left >= value - 1 else points_left
-                        setattr(self, self.stats_map[stat], points)
+                        self.dragon_stats[self.STATS_MAP[stat]] = points
                 points_left -= points
 
         elif weather_code == 'SRO':
             # Doesn't really matter what we do here - EVERYONE DIES!
-            self.scaleThickness = 20
-            self.clawSharpness = 0
-            self.wingStrength = 0
-            self.fireBreath = 0
+            self.set_dragon_stats(20, 0, 0, 0)
         else:
-            self.scaleThickness = 5
             # Additional claw-sharpening is needed to destroy the umbrellaboats.
-            self.clawSharpness = 10
-            self.wingStrength = 5
             # Fire is useless in the rain.
-            self.fireBreath = 0
+            self.set_dragon_stats(5, 10, 5, 0)
+
+    def set_dragon_stats(self, scale_thickness, claw_sharpness, wing_strength, fire_breath):
+        self.dragon_stats = {'scaleThickness': scale_thickness,
+                             'clawSharpness': claw_sharpness,
+                             'wingStrength': wing_strength,
+                             'fireBreath': fire_breath}
 
     def get_json(self):
-        return {"dragon": {
-                    "scaleThickness": self.scaleThickness,
-                    "clawSharpness": self.clawSharpness,
-                    "wingStrength": self.wingStrength,
-                    "fireBreath": self.fireBreath}}
+        return {"dragon": self.dragon_stats }
