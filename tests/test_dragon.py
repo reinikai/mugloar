df53a9b7ea2fe95031b19da603f03190669f0e8a
Jani Reinikainen <reinikai@users.noreply.github.com>
2017-05-17 18:14:50 +0300

    Implement test for dragon class

---
+++ tests/test_dragon.py
@@ -0,0 +1,7 @@
+import pytest
+from mugloar import dragon
+
+def test_partition():
+    for solution in dragon.partition(20, 4, 0, 10):
+        print (solution)
+        assert abs(solution[0]) + abs(solution[1]) + abs(solution[2]) + abs(solution[3]) == 20
