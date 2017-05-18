69f7cc9ea2669c360420247d3d06c3a761f22356
Jani Reinikainen <reinikai@users.noreply.github.com>
2017-05-18 17:54:04 +0300

    Implement type hinting and PEP8 formatting

--- mugloar/dragon.py
+++ mugloar/dragon.py
@@ -1,38 +1,39 @@
+from typing import Dict, Generator
+
 # Match knight's stats with dragon's stats.
 STATS_MAP = {'attack': 'scaleThickness',
-              'armor': 'clawSharpness',
-              'agility': 'wingStrength',
-              'endurance': 'fireBreath'}
+             'armor': 'clawSharpness',
+             'agility': 'wingStrength',
+             'endurance': 'fireBreath'}
 
 # Total number of points that can be divided amongst a dragon's stats.
-POINTS_AVAILABLE = 20
+POINTS_AVAILABLE: int = 20
 
 # Maximum number of points that can be given to a single stat.
 # Otherwise, "Dragon died of performance enhancement drug overdose"
-MAX_PER_STAT = 10
+MAX_PER_STAT: int = 10
 
 # The API does not allow one to assign negative points to a stat.
 # Otherwise: "Dragon developed anorexia and went chasing after a career as a runway model"
-MIN_PER_STAT = 0
+MIN_PER_STAT: int = 0
 
 
 class Dragon:
 
     def __init__(self):
-        """ Initializes instance variables """
-        self.dragon_stats = {}
+        """ Initializes instance variables. """
+        self.dragon_stats: Dict[str, int] = {}
 
-
-    def set_relative_stats(self, dragon_stats, knight):
-        """ Sets the dragon's stats relative to the knight's corresponding stats """
+    def set_relative_stats(self, dragon_stats: tuple, knight: dict) -> None:
+        """ Sets the dragon's stats relative to the knight's corresponding stats. """
 
         points_left = POINTS_AVAILABLE
 
         for i, (stat, value) in enumerate(knight):
             relative_points = value + dragon_stats[i]
 
-            if relative_points < 0:
-                points = 0
+            if relative_points < MIN_PER_STAT:
+                points = MIN_PER_STAT
             elif points_left >= relative_points:
                 points = relative_points
             else:
@@ -41,28 +42,29 @@ class Dragon:
             self.dragon_stats[STATS_MAP[stat]] = points
             points_left -= points
 
-    def set_stats(self, scale_thickness, claw_sharpness, wing_strength, fire_breath):
-        """ A shorthand method for setting all of the dragon's stats in one go """
+    def set_stats(self, scale_thickness: int, claw_sharpness: int, wing_strength: int, fire_breath: int) -> None:
+        """ A shorthand method for setting all of the dragon's stats in one go. """
 
         if max(scale_thickness, claw_sharpness, wing_strength, fire_breath) > MAX_PER_STAT:
-            raise ValueError("Max " + MAX_PER_STAT + " points/stat allowed.")
+            raise ValueError("Max " + str(MAX_PER_STAT) + " points/stat allowed.")
 
         self.dragon_stats = {'scaleThickness': scale_thickness,
                              'clawSharpness': claw_sharpness,
                              'wingStrength': wing_strength,
                              'fireBreath': fire_breath}
 
-    def get_json(self):
-        """ Returns a dict that corresponds to the API's expected JSON format """
-        return {"dragon": self.dragon_stats }
+    def get_json(self) -> dict:
+        """ Returns dragon's stats in a format that corresponds to the API's expected JSON format. """
+        return {"dragon": self.dragon_stats}
 
 
 def possible_solutions():
-    """ Generate all possible distributions of available points """
+    """ Generate all possible distributions of available points. """
     return partition(POINTS_AVAILABLE, len(STATS_MAP), MIN_PER_STAT, MAX_PER_STAT)
 
-def partition(integer, partition_length, min_size, max_size):
-    """ Invoke positive integer partitioning with minimum and maximum element limits """
+
+def partition(integer: int, partition_length: int, min_size: int, max_size: int) -> Generator:
+    """ Invoke positive integer partitioning with minimum and maximum element limits. """
     if partition_length < 1 or integer < 0:
         return
 
@@ -72,4 +74,4 @@ def partition(integer, partition_length, min_size, max_size):
         return
     for i in range(min_size, max_size + 1):
         for result in partition(integer - i, partition_length - 1, i, max_size):
-            yield result+(i,)
+            yield result + (i,)
--- mugloar/weather.py
+++ mugloar/weather.py
@@ -1,36 +1,28 @@
 import requests
 import defusedxml.ElementTree as ElemTree
 from api import Api
+from typing import Dict
 
 
 class Client(Api):
 
-    """Cheat sheet
-       NMR = normal weather = normal fights
-       FUNDEFINEDG = fog
-       HVA = rain
-       SRO = storm
-       T E = dry
-    """
-
-    game_id = None
-    weather = {}
-
-    def __init__(self, game_id):
+    def __init__(self, game_id: int):
+        """ Initializes instance variables. """
         if game_id < 1:
             raise RuntimeError("Invalid game ID")
         self.game_id = game_id
+        self.weather: Dict[str, str] = {}
 
-    def request(self):
+    def request(self) -> None:
         game_body = requests.get(self._base_url + '/weather/api/report/' + str(self.game_id)).text
         self.parse(game_body)
 
-    def parse(self, xml_string):
-        root = ElemTree.fromstring(xml_string)
+    def parse(self, xml: str) -> None:
+        root = ElemTree.fromstring(xml)
         self.weather = convert_to_dict(root)
 
 
-def convert_to_dict(xml_element):
+def convert_to_dict(xml_element) -> dict:
     dictionary = {}
     for child in xml_element:
         dictionary[child.tag] = child.text
