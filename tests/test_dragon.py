b5cd09fb3f7df9e3a1029d492fdb13ed6fc305a5
Jani Reinikainen <reinikai@users.noreply.github.com>
2017-06-24 15:21:11 +0300

    Implement tests for all methods in dragon class

--- tests/test_dragon.py
+++ tests/test_dragon.py
@@ -19,6 +19,26 @@ def dragon_stats():
 
 def test_set_relative_stats(dragon_instance, dragon_stats, knight):
     dragon_instance.set_relative_stats(dragon_stats, knight)
+    assert dragon_instance.dragon_stats['scaleThickness'] == 2
+    assert dragon_instance.dragon_stats['clawSharpness'] == 0
+    assert dragon_instance.dragon_stats['wingStrength'] == 0
+    assert dragon_instance.dragon_stats['fireBreath'] == 18
+
+
+def test_set_stats(dragon_instance, dragon_stats):
+    dragon_instance.set_stats(*dragon_stats)
+    assert dragon_instance.dragon_stats['scaleThickness'] == 10
+    assert dragon_instance.dragon_stats['clawSharpness'] == 10
+    assert dragon_instance.dragon_stats['wingStrength'] == 0
+    assert dragon_instance.dragon_stats['fireBreath'] == 0
+
+
+def test_get_json(dragon_instance, dragon_stats):
+    dragon_instance.set_stats(*dragon_stats)
+    assert dragon_instance.get_json() == {'dragon': {'scaleThickness': 10,
+                                                     'clawSharpness': 10,
+                                                     'wingStrength': 0,
+                                                     'fireBreath': 0}}
 
 
 def test_partition():
